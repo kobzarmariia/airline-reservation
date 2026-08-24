da25c3679f293e0dd6dbbb065d6a19955639527e
Maria Kobzar <mariia.kobzar@gmail.com>
2026-08-24 18:25:16 +0200

    refactor: Remove AppController and AppService, simplifying the application structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FlightModule } from './modules/flight/flight.module';
 
 @Module({
   imports: [EventEmitterModule.forRoot(), FlightModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
